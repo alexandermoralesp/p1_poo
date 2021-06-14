a3d457da75738c2706759c5b937eb9bdeb91c809
GU-U7 <71157869+GU-U7@users.noreply.github.com>
2021-06-14 14:49:43 -0500

    Mi version

---
+++ p1.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main(){
+        bool clave[15];
+        bool valido{true};
+        char btn;
+        for(int i =0; i<15; i++){
+                do{
+                cout<<"Boton rojo (R) o azul (A): ";
+                cin>>btn;
+                }
+                while(btn!='R' && btn!='A');
+                if(btn=='R') clave[i] = 1;
+                else clave[i]=0;
+
+
+        }
+
+        bool binario[5];
+        for(int i=2; i<15; i+=3){
+                if(clave[i]==clave[i-2]&&clave[i]==!clave[i-1]){
+                        if(clave[i]){
+                                binario[(i-2)/3] = 1;
+                        }
+                        else {
+                                binario[(i-2)/3] = 0;
+                        }
+                }
+                else{
+                        cout<<"Patron incorrecto"<<endl;
+                        return 0;
+                }
+        }
+
+	short numero=0, dos;
+	for(int i=4; i>-1; i--){
+		if(binario[4-i]){
+			dos=1;
+			for(int j=0; j<i; j++)
+				dos*=2;
+			numero+=dos;
+		}
+	}
+        
+        short primos[]{2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31};
+	short *pr = find(primos, primos+11, numero);
+	if(pr!=primos+11){
+		cout<<"Clave correcta"<<endl;
+		return 0;
+	}
+	cout<<"Clave incorrecta"<<endl;
+        return 0;
+}
